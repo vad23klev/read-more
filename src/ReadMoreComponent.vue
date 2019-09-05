e25211450b2ecd978226070d5d11b9057257675c
Vadim Klevtsov <vad23klev@gmail.com>
2019-09-05 11:57:57 +0400

    Правки

--- src/ReadMoreComponent.vue
+++ src/ReadMoreComponent.vue
@@ -1,6 +1,6 @@
 <template>
 	<div>
-		<p v-html="formattedString"></p>
+		<p>{{formattedString}}</p>
 		<span v-show="text.length > maxChars">
 			<a :href="link" id="readmore" v-show="!isReadMore" v-on:click="triggerReadMore($event, true)">{{moreStr}}</a>
 			<a :href="link" id="readmore" v-show="isReadMore" v-on:click="triggerReadMore($event, false)">{{lessStr}}</a>
@@ -42,9 +42,28 @@ export default {
   computed: {
     formattedString: function formattedString() {
       var val_container = this.text;
-
+      var indexStart = -1;
+      var indexEnd = -1;
       if (!this.isReadMore && this.text.length > this.maxChars) {
-        val_container = val_container.substring(0, this.maxChars) + "...";
+        indexStart = val_container.indexOf('<p>') > -1 ? val_container.indexOf('<p>') : this.text.length;
+        val_container = this.text;
+        val_container = val_container.substring(indexStart);
+        while (val_container.indexOf('<p>') > -1 && val_container.indexOf('<p>') <= this.maxChars) {
+          indexStart = val_container.indexOf('<p>');
+        }
+        indexEnd = val_container.indexOf('</p>') > -1 ? val_container.indexOf('</p>') : this.text.length;
+        val_container = this.text;
+        val_container = val_container.substring(indexEnd);
+        while (val_container.indexOf('</p>') > -1 && val_container.indexOf('</p>') <= this.maxChars) {
+          indexEnd = val_container.indexOf('</p>');
+        }
+        val_container = this.text;
+        if((indexStart > this.maxChars && indexEnd > this.maxChars) || (indexStart < indexEnd && indexEnd < this.maxChars)) {
+          return val_container.substring(0, this.maxChars) + "...";
+        }
+        if(indexStart > indexEnd && indexStart < this.maxChars) {
+          return val_container.substring(0, this.maxChars) + "...</p>";
+        }
       }
 
       return val_container;
